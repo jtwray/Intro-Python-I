442aa66eeaeaeb2e0ee0ab3ad766e199722b752b
tucker <tuckerwray84@gmail.com>
2020-07-10 21:37:29 -0400

    08_comprehensions-- for in if statement on the final list comprehension--wishwe had these in javascript-- an OSS to come ehhh-eehh?

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -35,11 +35,13 @@ print(y)
 # Use a list comprehension to create a list containing only the _even_ elements
 # the user entered into list x.
 
-x = input("Enter comma-separated numbers: ").split(',')
+
+jar = input("Enter comma-separated numbers: ").split(',')
+
 
 # What do you need between the square brackets to make it work?
-y = []
+y = [ cookie for (cookie) in jar if (int(cookie)%2)==0 ]
 
 print(y)
-print()
+print(jar)
 
